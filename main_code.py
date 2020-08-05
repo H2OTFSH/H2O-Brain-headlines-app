bf36619574930fb42b9dff494571d99cdc70e5f1
bass578 <harryhaywardevans@outlook.com>
2020-08-05 15:04:58 +0000

    App finished

--- main_code.py
+++ main_code.py
@@ -1,4 +1,13 @@
 from tkinter import *
 window = Tk()
 
-
+from tkinter import *
+def bAaction():
+  print('your brain headlines go here')
+def bBaction():
+  print('Made by H2O please copy but leave H2O in the discription H2O The Fun Starts Here and E n H Music app cade written by Harry Hayward Evans Idea By Orla ')
+window = Tk()
+buttonA = button(window, text='Brain Headlines', command=bAaction)
+buttonB = button(window, text='Infomation And Copyright', command=bBaction)
+buttonA.pack()
+buttonB.pack()
